434b41a3e1c84474fec277782cfacfce4d6cd8fd
sstom <sstom@users.noreply.github.com>
2020-02-23 19:24:50 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -50,7 +50,7 @@
             assessmentButton.onclick();
         }
     };
-
+    
 const answers = [
     '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
